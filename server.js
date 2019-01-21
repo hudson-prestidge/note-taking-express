5ccfa5495c27a1face1a8059d2361d1cfaa4c69c
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-01-21 15:24:11 +1300

    add text input, post route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.post('/', (req, res) => console.log('posted to index!'))
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.listen(3000)
