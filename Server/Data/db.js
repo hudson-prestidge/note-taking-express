c1a74171cbfbece311abe54a9b65677d2201787a
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-01-28 01:13:17 +1300

    add ability to select notes, styling of notes

--- Server/Data/db.js
+++ Server/Data/db.js
@@ -1,9 +1,9 @@
 var config = require('../../knexfile').development
 var knex = require('knex')(config)
 
-var getNotes = () => knex.select('id', 'content', 'time_posted').from('notes')
+var getNotes = () => knex.select('id', 'content', 'time_posted').from('notes
 
-var addNote = id => knex('notes').insert({id: id, content: ""})
+var addNote = (content) => knex('notes').insert({content: content})
 
 module.exports = {
   getNotes,
--- public/main.js
+++ public/main.js
@@ -1,15 +1,27 @@
+var clickNote = function() {
+  if (document.getElementById('selected-note')) {
+    document.getElementById('selected-note').removeAttribute('id')
+  }
+  this.setAttribute('id', 'selected-note')
+}
+
 var request = new XMLHttpRequest();
 
 request.open('GET', '/api/v1/notes', true)
 
 request.onload = function() {
  var data = JSON.parse(this.response);
- console.table(data);
  var activeNotes = document.getElementsByClassName('active-notes')[0]
  var currentNote;
  for(var i = 0; i < data.length; i++) {
    currentNote = activeNotes.appendChild(document.createElement("div"))
+   currentNote.classList.add('active-note')
    currentNote.innerHTML = data[i].content
+   currentNote.addEventListener('click', clickNote)
  }
 }
+
+
+
+
 request.send();
