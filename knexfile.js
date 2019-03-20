76e1373974252935b3cbd2c87b7ece5a53fc1d3b
Hudson Burns Prestidge <hudson.prestidge@gmail.com>
2019-03-20 15:49:06 +1300

    more heroku setup

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,13 @@
 // Update with your config settings.
-let herokuinfo = require('./herokuinfo')
 
 module.exports = {
     development: {
       client: 'pg',
       connection: {
-        host: herokuinfo.host,
-        database: herokuinfo.database,
-        user: herokuinfo.user,
-        password: herokuinfo.password,
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_URL,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
         ssl: true
       },
       pool: {
